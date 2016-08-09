0388ba2f74d66ddf5c0679ca47fcb24040a4c42c
Wonder Chang <iwonder.tw@gmail.com>
2016-08-09 16:36:49 +0800

    Migrate DB: Add Question model

--- main/models.py
+++ main/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Question(models.Model):
+  user = models.ForeignKey(User, unique=True)
+  title = models.CharField(max_length=200)
+  content = models.CharField(max_length=200)
+  pub_date = models.DateTimeField()
+  mod_date = models.DateTimeField()
+  active = models.BooleanField(default=True)
