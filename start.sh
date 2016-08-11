f1290052ec96af8e4ea9be4f79d4015574a677a2
dirty <darby.chang@gmail.com>
2016-08-11 22:42:15 +0800

    control the window after start

--- start.sh
+++ start.sh
@@ -5,4 +5,7 @@ tmux=/usr/local/bin/tmux
 cmd1="$npm run webpack"
 cmd2=". ~/.virtualenvs/mbi_so/bin/activate; $npm run django"
 
-$tmux new-window -c '#{pane_current_path}' -n server "$cmd1" \; split-window -h "$cmd2"
+# comment/un-comment `\; last-window` to control the window after servers start
+#     comment: stay in the servers window
+#  un-comment: back to the window you run this script
+$tmux new-window -c '#{pane_current_path}' -n servers "$cmd1" \; split-window -h "$cmd2" # \; last-window
