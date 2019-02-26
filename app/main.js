fcc63a42e2056dfbaa6800f28688b260757738e9
dirty <darby.chang@gmail.com>
2019-02-26 21:46:26 +0800

    use parcel

---
+++ app/main.js
@@ -0,0 +1,12 @@
+// modules
+import 'semantic-ui-offline/semantic.min'
+import 'semantic-ui-offline/semantic.min.css'
+import 'jquery-form/jquery.form.js'
+
+// our code
+import './main.styl'
+
+// our js code
+$(document).ready(() => {
+  console.log('it works')
+})
